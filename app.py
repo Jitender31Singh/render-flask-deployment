e8c2b77e94c59f9b52a0807dd929f73e7592221f
mahender singh <jitenderscool@gmail.com>
2024-11-06 19:38:45 +0530

    first commit

--- app.py
+++ app.py
@@ -2,31 +2,59 @@ from flask import Flask,request,jsonify
 import pickle
 import numpy as np
 
-model=pickle.load(open('model.pkl','rb'))
+# model=pickle.load(open('model.pkl','rb'))
+
+model =pickle.load(open('modelNew.pkl','rb'))
 
 app=Flask(__name__)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
-@app.route('/')
+@app.route('/home',methods=['GET'])
 def home():
     return "hello world"
+
+# @app.route('/predict',methods=['POST'])
+# def predict():
+#     age=int(request.form.get('age'))
+#     sex=int(request.form.get('sex'))
+#     cp=int(request.form.get('cp'))
+#     trestbps=int(request.form.get('trestbps'))
+#     chol=int(request.form.get('chol'))
+#     fbs=int(request.form.get('fbs'))
+#     restecg=int(request.form.get('restecg'))
+#     thalach=int(request.form.get('thalach'))
+#     exang=int(request.form.get('exang'))
+#     oldpeak=float(request.form.get('oldpeak'))
+#     slope=int(request.form.get('slope'))
+#     ca=int(request.form.get('ca'))
+#     thal=int(request.form.get('thal'))
+    
+#     input_query=np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
+   
+#     result=model.predict(input_query)[0]
+#     result = int(result)
+
+
 
 @app.route('/predict',methods=['POST'])
 def predict():
     age=int(request.form.get('age'))
     sex=int(request.form.get('sex'))
     cp=int(request.form.get('cp'))
-    trestbps=int(request.form.get('trestbps'))
+    # trestbps=int(request.form.get('trestbps'))
     chol=int(request.form.get('chol'))
-    fbs=int(request.form.get('fbs'))
+    # fbs=int(request.form.get('fbs'))
     restecg=int(request.form.get('restecg'))
     thalach=int(request.form.get('thalach'))
-    exang=int(request.form.get('exang'))
+    # exang=int(request.form.get('exang'))
     oldpeak=float(request.form.get('oldpeak'))
-    slope=int(request.form.get('slope'))
+    # slope=int(request.form.get('slope'))
     ca=int(request.form.get('ca'))
     thal=int(request.form.get('thal'))
     
-    input_query=np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
+    input_query=np.array([[age,sex,cp,chol,restecg,thalach,oldpeak,ca,thal]])
+   
     result=model.predict(input_query)[0]
     result = int(result)
     
@@ -38,3 +66,6 @@ def predict():
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
